d883fcd8e9ab16f14da2aec1ef7c17dc9b49a42f
David Aronchick <aronchick@gmail.com>
2020-05-25 21:44:47 +0000

    updating requirements

--- build_container.sh
+++ build_container.sh
@@ -2,11 +2,16 @@
 pipenv update mlspeclib
 pipenv lock -r > requirements.txt
 
+git add .
+git commit -a -m 'updating requirements'
+git push
+
 python3 -m unittest tests/tests_*
 RUN_TYPE=0
 python3 tests/run_main.py
 
 docker build --no-cache -t mlspec/mlspeclib-action-docker .
+docker push mlspec/mlspeclib-action-docker
 
 RUN_TYPE=3
 python3 tests/run_main.py
